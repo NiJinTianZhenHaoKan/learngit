16f832e7607af1d65be0446cec1ff7283f67f7bf
SMJ <503869311@qq.com>
2020-07-25 11:21:30 +0800

    new file

--- src/main/java/com/smj/learngit/HelloWorld.java
+++ src/main/java/com/smj/learngit/HelloWorld.java
@@ -5,4 +5,7 @@ package com.smj.learngit;
  * @date 2020-07-25 11:16
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello，World");
+    }
 }
